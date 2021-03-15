0894af414168fe0e8dbdeadd328331e1260faa4e
Patrick Perron <webpape@gmail.com>
2021-03-15 11:44:38 -0400

    Uncomments base api configuration basePath

--- templates/_request-signature.ts
+++ templates/_request-signature.ts
@@ -29,7 +29,7 @@ export interface IHeadersData {
 // Ezmax-Fingerprint: v1=9143277832f2905403c1245ce462ccf5ae23266bcb93f1f072d87394cec7d3a5
 // Ezmax-Signature: v1=071f141083f28003ef601cfe71c5e85f244dd9001df684befe55b5b6b2ecbd94
 
-export class RequestSignatureApi {
+export class RequestSignature {
   public static getFingerprint(fingerprintData: IFingerprintData): string {
     const contentToHash = `${fingerprintData.method}\n${fingerprintData.url}\n${fingerprintData.body}\n${fingerprintData.authorization}\n${fingerprintData.date}`
     const output: string = forge.md.sha256.create().update(contentToHash).digest().toHex()
