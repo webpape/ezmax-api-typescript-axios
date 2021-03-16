949a8c5735eb977cc4dca1842a249b254ba57df0
Patrick Perron <webpape@gmail.com>
2021-03-15 23:45:58 -0400

    Fix node-forge types and import

--- templates/._request-signature.ts
+++ templates/._request-signature.ts
@@ -1,7 +1,5 @@
-/* tslint:disable */
-/* eslint-disable */
-import forge from 'node-forge'
-
+import { md, hmac } from 'node-forge'
+  
 export interface IFingerprintData {
   authorization: string
   date: string
@@ -25,25 +23,21 @@ export interface IHeadersData {
   body: string
 }
 
-// Ezmax-Date: 2021-12-31T23:59:59Z
-// Ezmax-Fingerprint: v1=9143277832f2905403c1245ce462ccf5ae23266bcb93f1f072d87394cec7d3a5
-// Ezmax-Signature: v1=071f141083f28003ef601cfe71c5e85f244dd9001df684befe55b5b6b2ecbd94
-
 export class RequestSignature {
   public static getFingerprint(fingerprintData: IFingerprintData): string {
     const contentToHash = `${fingerprintData.method}\n${fingerprintData.url}\n${fingerprintData.body}\n${fingerprintData.authorization}\n${fingerprintData.date}`
-    const output: string = forge.md.sha256.create().update(contentToHash).digest().toHex()
+    const output: string = md.sha256.create().update(contentToHash).digest().toHex()
     return 'v1=' + output
   }
 
   public static getSignature(signatureData: ISignatureData): string {
     const contentToSign = `${signatureData.fingerprint}${signatureData.authorization}${signatureData.date}`
 
-    const hmac = forge.hmac.create()
-    hmac.start('sha512/256', signatureData.secret)
-    hmac.update(contentToSign)
+    const hmacObject = hmac.create()
+    hmacObject.start(<any>'sha512/256', signatureData.secret)
+    hmacObject.update(contentToSign)
 
-    const output: string = hmac.digest().toHex()
+    const output: string = hmacObject.digest().toHex()
 
     return 'v1=' + output
   }
